a37619f48baf4ea119f66fcedfded64ac8122ecc
AKToran <aktoran60@gmail.com>
2025-04-21 22:26:14 +0600

    add read list to local storage

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -1,49 +1,75 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router';
-import './bookDetails.css'
+import React from "react";
+import { useLoaderData, useParams } from "react-router";
+import "./bookDetails.css";
+import { addToStorage } from "../../utilities/handleStorage";
 
 const BookDetails = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const books = useLoaderData();
-  const book = books.find((b)=> b.bookId == id )
-  const { bookName, author, image, tags, rating, category, yearOfPublishing, publisher, totalPages, review } = book;
+  const book = books.find((b) => b.bookId == id);
+  const {
+    bookId,
+    bookName,
+    author,
+    image,
+    tags,
+    rating,
+    category,
+    yearOfPublishing,
+    publisher,
+    totalPages,
+    review,
+  } = book;
+
+
   return (
-    <div className='flex flex-col md:flex-row my-12'>
-      <div className='md:w-1/2 h-fit bg-base-300 rounded-2xl'>
-        <img className='p-20' src={image} alt="" />
+    <div className="flex flex-col md:flex-row my-12">
+      <div className="md:w-1/2 h-fit bg-base-300 rounded-2xl">
+        <img className="p-20" src={image} alt="" />
       </div>
-      <div className='p-8 md:w-1/2 space-y-6'>
-        <h1 className='text-4xl'>{bookName}</h1>
+      <div className="p-8 md:w-1/2 space-y-6">
+        <h1 className="text-4xl">{bookName}</h1>
         <p>By: {author}</p>
         <hr />
         <p>{category}</p>
         <hr />
         <p>Review: {review}</p>
-        <h4><span className='font-bold'>Tag:</span> {tags.map(((t, i)=> <span key={i} className='me-2'>{t}</span>))}</h4>
+        <h4>
+          <span className="font-bold">Tag:</span>{" "}
+          {tags.map((t, i) => (
+            <span key={i} className="me-2">
+              {t}
+            </span>
+          ))}
+        </h4>
         <hr />
         <table>
           <tbody>
             <tr>
-              <td className='pe-8'>Number of Pages:</td>
-              <td className='font-bold'>{totalPages}</td>
+              <td className="pe-8">Number of Pages:</td>
+              <td className="font-bold">{totalPages}</td>
             </tr>
             <tr>
-              <td className='pe-8'>Publisher:</td>
-              <td className='font-bold'>{publisher}</td>
+              <td className="pe-8">Publisher:</td>
+              <td className="font-bold">{publisher}</td>
             </tr>
             <tr>
-              <td className='pe-8'>Year of Publishing:</td>
-              <td className='font-bold'>{yearOfPublishing}</td>
+              <td className="pe-8">Year of Publishing:</td>
+              <td className="font-bold">{yearOfPublishing}</td>
             </tr>
             <tr>
-              <td className='pe-8'>Rating:</td>
-              <td className='font-bold'>{rating}</td>
+              <td className="pe-8">Rating:</td>
+              <td className="font-bold">{rating}</td>
             </tr>
           </tbody>
         </table>
 
-        <button className='btn btn-accent btn-outline me-4 mb-0'>Read Now</button>
-        <button className='btn btn-info'>Add To WishList</button>
+        <button onClick={() =>{
+          addToStorage(bookId)
+        }} className="btn btn-accent btn-outline me-4 mb-0">
+          Mark as Read
+        </button>
+        <button className="btn btn-info">Add To WishList</button>
       </div>
     </div>
   );
---
+++ src/utilities/handleStorage.js
@@ -0,0 +1,24 @@
+const getStoredBooks = () => {
+  const storedBooks = localStorage.getItem("readList");
+  let books;
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
+  } else {
+    books = [];
+  }
+  return books;
+};
+
+const addToStorage = (id) => {
+  const storedBooks = getStoredBooks();
+
+  if(storedBooks.includes(id)){
+    alert("Book already exits.")
+  }
+  else{
+    storedBooks.push(id);
+    localStorage.setItem('readList', JSON.stringify(storedBooks));
+  }
+};
+
+export { addToStorage };
