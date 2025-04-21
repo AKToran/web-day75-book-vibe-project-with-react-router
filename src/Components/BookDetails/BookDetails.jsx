51592fa4393f0c8f4f7e35b8babed6d0d8bc0fc4
AKToran <aktoran60@gmail.com>
2025-04-21 21:02:16 +0600

    book details page done

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useLoaderData, useParams } from 'react-router';
+import './bookDetails.css'
 
 const BookDetails = () => {
-  const {id} = useParams()
-  console.log(id);
+  const {id} = useParams();
+  const books = useLoaderData();
+  const book = books.find((b)=> b.bookId == id )
+  const { bookName, author, image, tags, rating, category, yearOfPublishing, publisher, totalPages, review } = book;
   return (
-    <div>
-      details
+    <div className='flex flex-col md:flex-row my-12'>
+      <div className='md:w-1/2 h-fit bg-base-300 rounded-2xl'>
+        <img className='p-20' src={image} alt="" />
+      </div>
+      <div className='p-8 md:w-1/2 space-y-6'>
+        <h1 className='text-4xl'>{bookName}</h1>
+        <p>By: {author}</p>
+        <hr />
+        <p>{category}</p>
+        <hr />
+        <p>Review: {review}</p>
+        <h4><span className='font-bold'>Tag:</span> {tags.map(((t, i)=> <span key={i} className='me-2'>{t}</span>))}</h4>
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td className='pe-8'>Number of Pages:</td>
+              <td className='font-bold'>{totalPages}</td>
+            </tr>
+            <tr>
+              <td className='pe-8'>Publisher:</td>
+              <td className='font-bold'>{publisher}</td>
+            </tr>
+            <tr>
+              <td className='pe-8'>Year of Publishing:</td>
+              <td className='font-bold'>{yearOfPublishing}</td>
+            </tr>
+            <tr>
+              <td className='pe-8'>Rating:</td>
+              <td className='font-bold'>{rating}</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <button className='btn btn-accent btn-outline me-4 mb-0'>Read Now</button>
+        <button className='btn btn-info'>Add To WishList</button>
+      </div>
     </div>
   );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -19,6 +19,7 @@ export const router = createBrowserRouter([
       },
       {
         path:'/bookDetails/:id',
+        loader: () => fetch('bookData.json'),
         Component: BookDetails
       },
       {
